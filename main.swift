4b6dc3ecf243250619ea00624a43199e486e1aac
lzh11 <2239040999@qq.com>
2016-06-20 10:57:47 +0800

    L007Loop
    先定义一个int类变量i＝1，再定义一个变量a＝1，然后再用while语句进行循环，条件设置成i小于100，然后进行循环，把值赋给a，最后输出a
    ，得到结果。

---
+++ main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  L007Loop
+//
+//  Created by LZH on 16/6/20.
+//  Copyright © 2016年 LZH. All rights reserved.
+//
+
+import Foundation
+
+var i = 1
+var a = 1
+while i<100{
+    
+i++
+a = a + i
+}
+print(a)
+
